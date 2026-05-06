ffc97d9e6b60227b86cee7e5ab0c64d1fbf19411
w15hy <ingw15hy@gmail.com>
2026-05-06 16:10:57 -0500

    utils, otra vez

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -3,7 +3,18 @@ def ofile(file):
         content = file.read()
         return content
 
+
 def ofilelines(file):
     with open(file, "r") as file:
         content = file.readlines()
         return content
+
+
+def wfilelines(file, lineas):
+    with open(file, "w", encoding="utf-8") as f:
+        f.writelines(lineas)
+
+
+def wfile(tok):
+    with open("tokens.txt", "a", encoding="utf-8") as f:
+        f.write(f"{tok}\n")
